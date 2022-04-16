6d7bae3aef549fc4da32680805857e8b84a93542
dominhnha <dominhnhat.dlc@gmail.com>
2022-04-16 21:28:24 +0700

    update full project

---
+++ .history/assets/JS/app_20220414232333.js
@@ -0,0 +1,27 @@
+let checkevent = false;
+window.onscroll = function()
+{
+    myFuction();
+    return;
+
+}
+function myFuction(){
+    let navbar = document.getElementById("myNavbar");
+    let card_list = document.getElementsByClassName("header__list-link");
+    navbar.className = "";
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop >100){
+        if(document.body.scrollTop > 600 || document.documentElement.scrollTop >600){
+            return;
+        }
+        navbar.className += 'header__list BGR-white animate-top';
+        for(let i = 0; i < card_list.length; i++){
+            card_list[i].className =`header__list-link cl-black`;
+        }  
+    }else{
+        navbar.className += 'header__list BGR-black';
+        for(let i = 0; i < card_list.length; i++){
+            card_list[i].className =`header__list-link cl-while`;
+        }
+    }
+    
+}
